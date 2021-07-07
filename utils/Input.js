a2a1707d7d35c4ea8c3ecf230cb1bfb2d14f796d
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-07 05:48:11 -0500

    Add files via upload

---
+++ utils/Input.js
@@ -0,0 +1,17 @@
+module.exports = function(obj, ...properties) {
+    const errors = [];
+  
+    properties.forEach(property => {
+      // if property is blank or doesn't exist, add to errors array
+      if (obj[property] === undefined || obj[property] === '') {
+        errors.push(`No ${property} specified.`);
+      }
+    });
+  
+    if (errors.length) {
+      return {
+        error: errors.join(' ')
+      };
+    }
+    return null;
+  };
